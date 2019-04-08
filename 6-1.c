84d2e8b52ae939bd71ca5da9f546f46a98e9a3a5
livil <henrivonzeiss@gmail.com>
2019-04-08 14:06:35 +0800

    Create 6-1.c

---
+++ 6-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void PrintN ( int N );
+
+int main ()
+{
+    int N;
+
+    scanf("%d", &N);
+    PrintN( N );
+
+    return 0;
+}
+
+
+/* 你的代码将被嵌在这里 *
+
+void PrintN(int N){
+int i;
+for(i = 1;i <=N;i++)
+{
+  printf("%d\n",i);
+}
+return 0;
+}
